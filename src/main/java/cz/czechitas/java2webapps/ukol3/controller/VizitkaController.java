545979f6d4df259f2e5eb5adf80a22ea5db26d8d
Eva Marko <valentova.eva92@gmail.com>
2023-10-15 23:00:58 +0200

    Added entity Vizitka and VizitkaController finished
    Added entity Vizitka with all parametres. VizitkaController finished with list, method seznam returning ModelAndView list and method detail returning detail of items from the list. seznam.html and detail.html were rewritten so now these templates are showing data from model.

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -1,20 +1,39 @@
 package cz.czechitas.java2webapps.ukol3.controller;
 
+import cz.czechitas.java2webapps.ukol3.entity.Vizitka;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * Kontroler obsluhující zobrazování vizitek.
  */
 @Controller
 public class VizitkaController {
+
+  private final List<Vizitka> seznamVizitek = List.of(
+          new Vizitka("Dita (Přikrylová) Formánková", "Czechitas z. s.", "Václavské náměstí 837/11", "11000  Praha 1", "dita@czechitas.cs", "+420 800123456", "www.czechitas.cz"),
+          new Vizitka("Barbora Bühnová", "Czechitas z. s.", "Václavské náměstí 837/11", "11000  Praha 1", null, "+420 800123456", "www.czechitas.cz"),
+          new Vizitka("Monika Ptáčníková", "Czechitas z. s.", "Václavské náměstí 837/11", "11000  Praha 1", "monika@czechitas.cs", "+420 800123456", "www.czechitas.cz"),
+          new Vizitka("Mirka Zatloukalová", "Czechitas z. s.", "Václavské náměstí 837/11", "11000  Praha 1", "mirka@czechitas.cs", null, "www.czechitas.cz")
+  );
+
   @GetMapping("/")
-  public String seznam() {
-    return "seznam";
+  public ModelAndView seznam() {
+    ModelAndView modelAndView = new ModelAndView("seznam");
+    modelAndView.addObject("vizitky", seznamVizitek);
+    return modelAndView;
   }
 
-  @GetMapping("/detail")
-  public String detail() {
-    return "detail";
+  @GetMapping("/detail/{id}")
+  public ModelAndView detail(@PathVariable int id) {
+    ModelAndView modelAndView = new ModelAndView("detail");
+    modelAndView.addObject("vizitka", seznamVizitek.get(id));
+    return modelAndView;
   }
 }
+
+
